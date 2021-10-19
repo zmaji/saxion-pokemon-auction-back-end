b45478e488be911938e6b1a81a3d99994aca7eb7
NKimenai <nilskimenai@hotmail.nl>
2021-10-19 10:51:23 +0200

    cors

--- src/index.js
+++ src/index.js
@@ -20,7 +20,7 @@ app.use(express.static('public'));
 
 app.use(function(req, res, next) {
     res.header("Access-Control-Allow-Origin", "*");
-    res.header("Access-Control-Allow-Methods", "GET,HEAD,OPTIONS,POST,PUT");
+    res.header("Access-Control-Allow-Methods", "GET,HEAD,OPTIONS,POST,PUT,DELETE");
     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization");
     next();
 });
